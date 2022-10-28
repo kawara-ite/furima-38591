02a9838ba41efedcc7e98f3afce1ac54676ebf71
kawara-ite <114914557+kawara-ite@users.noreply.github.com>
2022-10-28 23:00:27 +0900

    アクションとルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,21 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+  end
 
+  def create
+    @item = item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :info, :price, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :image).merge(user_id: current_user.id)
   end
 end
