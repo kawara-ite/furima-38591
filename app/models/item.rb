eda8bde9cce345e5e800368f5cfde18719ead6a1
kawara-ite <114914557+kawara-ite@users.noreply.github.com>
2022-10-28 22:39:01 +0900

    itemモデルにバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,14 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   # has_one :order
+
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :price, presence: true
+  validates :category_id, presence: true
+  validates :sales_status_id, presence: true
+  validates :shipping_fee_status_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :scheduled_delivery_id, presence: true
+  validates :image, presence: true
 end
