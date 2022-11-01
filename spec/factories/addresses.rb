4c30fc272f1da72160c6f786dfdd242842871bba
kawara-ite <114914557+kawara-ite@users.noreply.github.com>
2022-11-01 19:52:30 +0900

    テストコード途中

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :address do
+    association :user, :item
+    postal_code                   { 123-4567 }
+    prefecture_id          { Faker::Number.between(from: 2, to: 49) }
+    city                  { Faker::Address.city }
+    address            { Faker::Address.secondary_address }
+    phone_number        { '09012345678' }
     
   end
 end
