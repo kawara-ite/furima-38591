d43b62be6cb48a3e2dfcb631e1c6ae597796a4d1
kawara-ite <114914557+kawara-ite@users.noreply.github.com>
2022-11-02 19:12:47 +0900

    遷移コード追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    if @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def update
@@ -43,7 +46,7 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :info, :price, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id,
-                                 :scheduled_delivery_id, :image).merge(user_id: current_user.id)
+                                 :scheduled_delivery_id, :image).merge(user_id: current_user.id, order_id: params[:order_id])
   end
 
   def move_to_index
